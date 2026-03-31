8b60b18b90a8a80e770cda10c0cc79d04dd5fd81
psallescard <pesalcardoso@gmail.com>
2026-03-31 04:31:44 -0300

    :recycle: refactor: Centraliza o código da navbar em 3 arquivos

--- src/ui/widgets/navbar/subnav.py
+++ src/ui/widgets/navbar/subnav.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import flet as ft
 
-from .buttons import BottomLineTextButton
+from ui.styles import AppColors
 
 
 class SubNav(ft.Row):
@@ -31,3 +31,50 @@ class SubNav(ft.Row):
             else:
                 tab.deactivate()
             tab.update()
+
+
+class BottomLineTextButton(ft.Column):
+    def __init__(self, text: str, icon: ft.IconData, on_change: callable) -> None:
+        super().__init__()
+        self.on_change = on_change
+        self.active_color = AppColors.BG_800
+        self.inactive_color = AppColors.BG_500
+        self.text = ft.Text(text, size=12, color=self.inactive_color)
+
+        self.component = ft.Container(
+            content=ft.TextButton(
+                content=self.text,
+                icon=icon,
+                icon_color=self.inactive_color,
+                style=ft.ButtonStyle(
+                    padding=ft.Padding.only(bottom=20, top=16, left=12, right=12),
+                    shape=ft.RoundedRectangleBorder(radius=5),
+                ),
+                on_click=self._on_click_handler,
+            ),
+            border=None,
+        )
+
+        self.controls = [
+            self.component,
+        ]
+
+    def _on_click_handler(self, e) -> None:  # noqa: ANN001, ARG002
+        """Pass the event to the father component to ensure all other tabs are deactivated."""
+        if self.on_change:
+            self.on_change(self)
+
+    def activate(self) -> None:
+        self.component.border = ft.Border.only(
+            # Use BorderSideStrokeAllign.OUTSIDE to prevent layout shift on activation/deactivation
+            bottom=(ft.BorderSide(width=2, color=AppColors.PRIMARY_500, stroke_align=ft.BorderSideStrokeAlign.OUTSIDE)),
+        )
+        self.text.color = self.active_color
+        self.component.content.icon_color = self.active_color
+        self.text.weight = ft.FontWeight.W_600
+
+    def deactivate(self) -> None:
+        self.component.border = None
+        self.text.color = self.inactive_color
+        self.component.content.icon_color = self.inactive_color
+        self.text.weight = ft.FontWeight.NORMAL
