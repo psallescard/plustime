545d3d32058ad9f3077374e73911fb2e13b9a664
psallescard <pesalcardoso@gmail.com>
2026-04-01 17:09:26 -0300

    :art: feat: Indroduz classes de temas pré-definidos

---
+++ src/ui/themes.py
@@ -0,0 +1,53 @@
+class Theme:
+    BG_50: str
+    BG_100: str
+    BG_300: str
+    BG_400: str
+    BG_500: str
+    BG_600: str
+    BG_800: str
+    BG_900: str
+
+
+class LightTheme(Theme):
+    BG_50 = "#F8FAFC"
+    BG_100 = "#E7EBEE"
+    BG_300 = "#CBD5E1"
+    BG_400 = "#94A3B8"
+    BG_500 = "#64748B"
+    BG_600 = "#475569"
+    BG_800 = "#1E293B"
+    BG_900 = "#0F172A"
+
+
+class DarkTheme(Theme):
+    BG_50 = "#0F172A"
+    BG_100 = "#1E293B"
+    BG_300 = "#334155"
+    BG_400 = "#475569"
+    BG_500 = "#64748B"
+    BG_600 = "#94A3B8"
+    BG_800 = "#CBD5E1"
+    BG_900 = "#F1F5F9"
+
+
+class PurpleTheme(Theme):
+    BG_50 = "#F5F3FF"
+    BG_100 = "#EBE9FE"
+    BG_300 = "#D9D6FE"
+    BG_400 = "#A6A4FA"
+    BG_500 = "#7974FA"
+    BG_600 = "#5048E5"
+    BG_800 = "#3F29B0"
+    BG_900 = "#201A7E"
+
+
+class BeigeTheme(Theme):
+    BG_50 = "#FDF8F0"
+    BG_100 = "#F7EEDD"
+    BG_300 = "#EADDC2"
+    BG_400 = "#DBC8A6"
+    BG_500 = "#C7B38F"
+    BG_600 = "#B09C7A"
+    BG_800 = "#8A765A"
+    BG_900 = "#5C4E3D"
